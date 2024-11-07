1432ac243cb861fa0ebbd2a3019d839bf2b9e9b8
Thee-alpha89 <trapcoholic1738@gmail.com>
2024-11-07 20:38:45 +0300

    Add files via upload

---
+++ CAT 2.Q2.c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    // Define an array data structure
+    int scores [2][2];
+    int i,j;
+
+    // Declare and initialize a 2D array named scores
+    scores[0][0] = 65;
+    scores[0][1] = 92;
+    scores[1][0] = 84;
+    scores[1][1] = 72;
+// Print the elements of the array using a nested for loop
+    for (i = 0; i <2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", scores[i][j]);
+        }
+        printf("\n");   
+    }
+    scores[0][0] = 35;
+    scores[0][1] = 70;
+    scores[1][0] = 59;
+    scores[1][1] = 67;
+
+    // Print the elements of the array using a nested for loop
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", scores[i][j]);
+        }
+         printf("\n");
+    }
+	return 0;
+ 
+}
---
+++ CAT 2.Q3.c.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	float hoursworked;
+	float hourlywage;
+	float grosspay;
+	float overtimepay;20
+	float taxes;
+	float netpay;
+		printf("enter hoursworked:");
+	scanf("%f",&hoursworked);
+	printf("enter hourlywage:");
+	scanf("%f",&hourlywage);
+	//grosspay
+	if(hoursworked<=40){
+		grosspay=hoursworked*hourlywage;
+	}else{
+		grosspay=(40*hourlywage)+(hoursworked-40)*(1.5*hourlywage);
+		}
+		//taxes
+	if(grosspay<=600){
+	taxes=0.15*grosspay;
+		}else{
+			taxes=(0.15*600)+(grosspay-600)*0.20;
+		}
+		//netpay
+		netpay=grosspay-taxes;
+		printf("grosspay:%.2f\n",grosspay);
+		printf("taxes:%.2f\n",taxes);
+		printf("netpay:%.2f\n",netpay);
+	return 0;
+}
